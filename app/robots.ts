bc196ae4b34e0cb5309b16b07a195dde935cf8e4
owenchang1992 <63446591+owenchang1992@users.noreply.github.com>
2024-05-04 13:59:13 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ app/robots.ts
@@ -0,0 +1,11 @@
+export default function robots() {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+      },
+    ],
+    sitemap: 'https://chinche-chang.xyz/sitemap.xml',
+    host: 'https://chinche-chang.xyz',
+  };
+}
